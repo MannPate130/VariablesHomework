5ad706898faa730bd5cb1ebc61410ebbcc0c245c
Mann Name <mannhp1103@outlook.com>
2020-11-25 09:18:25 -0500

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class variablesHomepage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.wayneGretzky = new System.Windows.Forms.Button();
+            this.area = new System.Windows.Forms.Button();
+            this.carpet = new System.Windows.Forms.Button();
+            this.bill = new System.Windows.Forms.Button();
+            this.forecolorLabel = new System.Windows.Forms.Label();
+            this.hockeyLabel = new System.Windows.Forms.Label();
+            this.areaLabel = new System.Windows.Forms.Label();
+            this.carpetLabel = new System.Windows.Forms.Label();
+            this.billLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // wayneGretzky
+            // 
+            this.wayneGretzky.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.wayneGretzky.Location = new System.Drawing.Point(12, 12);
+            this.wayneGretzky.Name = "wayneGretzky";
+            this.wayneGretzky.Size = new System.Drawing.Size(225, 74);
+            this.wayneGretzky.TabIndex = 0;
+            this.wayneGretzky.Text = "Hockey";
+            this.wayneGretzky.UseVisualStyleBackColor = true;
+            this.wayneGretzky.Click += new System.EventHandler(this.wayneGretzky_Click);
+            // 
+            // area
+            // 
+            this.area.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.area.Location = new System.Drawing.Point(12, 127);
+            this.area.Name = "area";
+            this.area.Size = new System.Drawing.Size(225, 73);
+            this.area.TabIndex = 1;
+            this.area.Text = "Area";
+            this.area.UseVisualStyleBackColor = true;
+            this.area.Click += new System.EventHandler(this.area_Click);
+            // 
+            // carpet
+            // 
+            this.carpet.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.carpet.Location = new System.Drawing.Point(12, 235);
+            this.carpet.Name = "carpet";
+            this.carpet.Size = new System.Drawing.Size(225, 70);
+            this.carpet.TabIndex = 2;
+            this.carpet.Text = "Carpet";
+            this.carpet.UseVisualStyleBackColor = true;
+            this.carpet.Click += new System.EventHandler(this.carpet_Click);
+            // 
+            // bill
+            // 
+            this.bill.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bill.Location = new System.Drawing.Point(12, 337);
+            this.bill.Name = "bill";
+            this.bill.Size = new System.Drawing.Size(225, 68);
+            this.bill.TabIndex = 3;
+            this.bill.Text = "Bill";
+            this.bill.UseVisualStyleBackColor = true;
+            this.bill.Click += new System.EventHandler(this.bill_Click);
+            // 
+            // forecolorLabel
+            // 
+            this.forecolorLabel.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.forecolorLabel.Location = new System.Drawing.Point(280, 9);
+            this.forecolorLabel.Name = "forecolorLabel";
+            this.forecolorLabel.Size = new System.Drawing.Size(490, 393);
+            this.forecolorLabel.TabIndex = 4;
+            this.forecolorLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // hockeyLabel
+            // 
+            this.hockeyLabel.BackColor = System.Drawing.Color.White;
+            this.hockeyLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hockeyLabel.Location = new System.Drawing.Point(290, 12);
+            this.hockeyLabel.Name = "hockeyLabel";
+            this.hockeyLabel.Size = new System.Drawing.Size(438, 84);
+            this.hockeyLabel.TabIndex = 5;
+            this.hockeyLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.hockeyLabel.UseMnemonic = false;
+            // 
+            // areaLabel
+            // 
+            this.areaLabel.BackColor = System.Drawing.Color.White;
+            this.areaLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.areaLabel.Location = new System.Drawing.Point(290, 107);
+            this.areaLabel.Name = "areaLabel";
+            this.areaLabel.Size = new System.Drawing.Size(433, 80);
+            this.areaLabel.TabIndex = 6;
+            // 
+            // carpetLabel
+            // 
+            this.carpetLabel.BackColor = System.Drawing.Color.White;
+            this.carpetLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.carpetLabel.Location = new System.Drawing.Point(291, 195);
+            this.carpetLabel.Name = "carpetLabel";
+            this.carpetLabel.Size = new System.Drawing.Size(407, 75);
+            this.carpetLabel.TabIndex = 7;
+            // 
+            // billLabel
+            // 
+            this.billLabel.BackColor = System.Drawing.Color.White;
+            this.billLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.billLabel.Location = new System.Drawing.Point(488, 265);
+            this.billLabel.Name = "billLabel";
+            this.billLabel.Size = new System.Drawing.Size(113, 126);
+            this.billLabel.TabIndex = 8;
+            // 
+            // variablesHomepage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.billLabel);
+            this.Controls.Add(this.carpetLabel);
+            this.Controls.Add(this.areaLabel);
+            this.Controls.Add(this.hockeyLabel);
+            this.Controls.Add(this.forecolorLabel);
+            this.Controls.Add(this.bill);
+            this.Controls.Add(this.carpet);
+            this.Controls.Add(this.area);
+            this.Controls.Add(this.wayneGretzky);
+            this.Name = "variablesHomepage";
+            this.Text = "VariablesHomePage";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button wayneGretzky;
+        private System.Windows.Forms.Button area;
+        private System.Windows.Forms.Button carpet;
+        private System.Windows.Forms.Button bill;
+        private System.Windows.Forms.Label forecolorLabel;
+        private System.Windows.Forms.Label hockeyLabel;
+        private System.Windows.Forms.Label areaLabel;
+        private System.Windows.Forms.Label carpetLabel;
+        private System.Windows.Forms.Label billLabel;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class variablesHomepage : Form
+    {
+        public variablesHomepage()
+        {
+            InitializeComponent();
+        }
+
+        private void wayneGretzky_Click(object sender, EventArgs e)
+        {
+            
+            carpetLabel.Visible = false;
+            billLabel.Visible = false;
+            areaLabel.Visible = false;
+            hockeyLabel.Visible = true;
+            string name = "Wayne Gretzky";
+            int playernumber = 99;
+            hockeyLabel.Text = $"\n{name} is number {playernumber}";
+            
+            
+           
+           
+        }
+
+        private void carpet_Click(object sender, EventArgs e)
+        {
+            hockeyLabel.Visible = false;
+            billLabel.Visible = false;
+            areaLabel.Visible = false;
+            carpetLabel.Visible = true;
+            double length = 8.5;
+            double width = 6;
+            double costPerMeter = 19.95;
+            double Area = length * width;
+            double totalCost = costPerMeter * Area;
+           
+            carpetLabel.Text = $"The area of a room with dimensions {length}m x {width}m is {Area}m^2 \nThe cost to carpet this area at {costPerMeter} per square metre is {totalCost}.";
+        }
+
+        private void area_Click(object sender, EventArgs e)
+        {
+            hockeyLabel.Visible = false;
+            billLabel.Visible = false;
+            carpetLabel.Visible = false;
+            areaLabel.Visible = true;
+            double radius = 15;
+            double pi = 3.14;
+            double area = pi * radius * radius;
+            areaLabel.Text = $"The area of a circle with a {radius}cm radius is: {area}cm^2";
+        }
+
+        private void bill_Click(object sender, EventArgs e)
+        {
+            hockeyLabel.Visible = false;
+            areaLabel.Visible = false;
+            carpetLabel.Visible = false;
+            billLabel.Visible = true;
+            string name = "Bill of Sale";
+            double shirt = 12.5;
+            double tax = 1.6;
+            double total = shirt + tax;
+            double tendered = 20.0;
+            double change = tendered - total;
+            billLabel.Text = $"{name}";
+         
+            billLabel.Text = $"{name}\nShirt:     {shirt} \nTax:      {1.6}\nTotal:     {total}\nTendered: { tendered}\nChange:    {change}";
+
+        }
+
+        // private void carpet_Click(object sender, EventArgs e)
+
+
+    }
+    }
+ 
